e81eb14ec2fa75e38c7d4aef466f07cb42d50645
theguffs <youssefabdelmalak01@gmail.com>
2024-09-02 16:19:24 +0200

    esercizio base

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message : "hello Vue"
+            emails: [] // Array email
         }
-    }
+    },
+    created() {
+        // Ciclo per 10 email
+        for (let i = 0; i < 10; i++) {
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+              this.emails.push(response.data.response);
+            })
+        }
+      }
 }).mount('#app')
